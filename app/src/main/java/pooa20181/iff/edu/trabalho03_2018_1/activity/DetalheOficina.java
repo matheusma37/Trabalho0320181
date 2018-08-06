97a2ea8435f19a627455ae6cf03bd986aa53c620
matheusma37 <mtsma37@gmail.com>
2018-08-06 18:22:54 -0300

    Criando activity_detalhe_oficina;

---
+++ app/src/main/java/pooa20181/iff/edu/trabalho03_2018_1/activity/DetalheOficina.java
@@ -0,0 +1,15 @@
+package pooa20181.iff.edu.trabalho03_2018_1.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pooa20181.iff.edu.trabalho03_2018_1.R;
+
+public class DetalheOficina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_oficina);
+    }
+}
